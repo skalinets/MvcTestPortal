348c90ad5e46f92f258e08da00705bf69f3c2d93
Serhiy Kalinets <kalinets@gmail.com>
2014-02-05 11:41:30 +0200

    adds db tests, adds migration

--- src/Tests/Class1.cs
+++ src/Tests/Class1.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
 
 namespace Tests
 {
-    public class Class1
+    
+    public class EntityTests
     {
+        [Fact]
+        public void hello()
+        {
+            var blog = new Blog {Name = "this is the blog"};
+            var bloggingContext = new BloggingContext();
+            bloggingContext.Blogs.Add(blog);
+            bloggingContext.SaveChanges();
+        }
+
+        [Fact]
+        public void read_data()
+        {
+            var bloggingContext = new BloggingContext();
+            bloggingContext.Blogs
+                .Where(b => b.Name.Contains("foo")).Should().BeEmpty();
+            bloggingContext.Blogs
+                .Where(b => b.Name.Contains("the")).Should().NotBeEmpty();
+
+//            bloggingContext.Blogs.Should().NotBeEmpty();
+        }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; } 
+    }
+
+    public class Blog
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
     }
 }
---
+++ src/Tests/Migrations/201402050928197_InitialCreate.cs
@@ -0,0 +1,26 @@
+namespace Tests.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Blogs",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Blogs");
+        }
+    }
+}
