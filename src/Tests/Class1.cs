31ef88669462e2fa3ecb4b388f62cbd80d738a98
Serhiy Kalinets <kalinets@gmail.com>
2014-02-05 11:48:51 +0200

    adds url property

--- src/Tests/Class1.cs
+++ src/Tests/Class1.cs
@@ -1,39 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
-using FluentAssertions;
 
 namespace Tests
 {
-    
-    public class EntityTests
-    {
-        [Fact]
-        public void hello()
-        {
-            var blog = new Blog {Name = "this is the blog"};
-            var bloggingContext = new BloggingContext();
-            bloggingContext.Blogs.Add(blog);
-            bloggingContext.SaveChanges();
-        }
-
-        [Fact]
-        public void read_data()
-        {
-            var bloggingContext = new BloggingContext();
-            bloggingContext.Blogs
-                .Where(b => b.Name.Contains("foo")).Should().BeEmpty();
-            bloggingContext.Blogs
-                .Where(b => b.Name.Contains("the")).Should().NotBeEmpty();
-
-//            bloggingContext.Blogs.Should().NotBeEmpty();
-        }
-    }
-
     public class BloggingContext : DbContext
     {
         public DbSet<Blog> Blogs { get; set; } 
@@ -43,5 +15,7 @@ namespace Tests
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+
+        public string Url { get; set; }
     }
 }
---
+++ src/Tests/EntityTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class EntityTests
+    {
+        [Fact]
+        public void hello()
+        {
+            var blog = new Blog {Name = "this is the blog"};
+            var bloggingContext = new BloggingContext();
+            bloggingContext.Blogs.Add(blog);
+            bloggingContext.SaveChanges();
+        }
+
+        [Fact]
+        public void read_data()
+        {
+            var bloggingContext = new BloggingContext();
+            bloggingContext.Blogs
+                           .Where(b => b.Name.Contains("foo")).Should().BeEmpty();
+            bloggingContext.Blogs
+                           .Where(b => b.Name.Contains("the")).Should().NotBeEmpty();
+
+//            bloggingContext.Blogs.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void try_first_migration()
+        {
+            var bloggingContext = new BloggingContext();
+            var blog = bloggingContext.Blogs.First();
+            blog.Url = "this is my URL";
+            bloggingContext.SaveChanges();
+
+        }
+    }
+}
